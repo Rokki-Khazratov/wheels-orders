bd58b1f4572cd521f5233a2855e0cda74d252fa9
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-04-14 00:31:55 +0500

    take images and send

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ bot = telebot.TeleBot(bot_token)
 API_ENDPOINT = 'http://127.0.0.1:8000/api/'
 CHANNEL = ADMINS_CHANNEL_ID
 
+@bot.message_handler(commands=['start'])
+def send_hello(message):
+    bot.send_message(message.chat.id, "Hello! Send /orders to fetch and send orders to the channel.")
+
+
+
 def get_orders():
     try:
         response = requests.get(API_ENDPOINT + 'orders/')
@@ -21,13 +27,19 @@ def get_orders():
         print("Error fetching orders:", e)
         return []
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=['orders'])
 def send_orders_to_channel(message):
     try:
         orders = get_orders()
         for order in orders:
+            image_url = order.get('passport_image', '')
+            image_filename = f"order_{order['id']}.jpg"
+            image_data = requests.get(image_url).content if image_url else None
+
             order_message = f"ID: {order['id']}\nName: {order['full_name']}\nPhone: {order['phone_number']}\nAddress: {order['adress']}\nChecked: {'Yes' if order['is_checked'] else 'No'}"
-            bot.send_message(CHANNEL, order_message)
+
+            bot.send_photo(CHANNEL, image_data, caption=order_message, parse_mode='Markdown')
     except Exception as e:
         print("Error:", e)
 
