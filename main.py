f39ec661a923a72469e57cb69772657a0ec045e5
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-04-13 23:55:20 +0500

    add plan todo

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import os
+import time
 import telebot
-from .keys import BOT_TOKEN
+from telebot import types
+from keys import BOT_TOKEN, ADMINS_CHANNEL_ID
 
-bot = BOT_TOKEN
+bot_token = BOT_TOKEN
+bot = telebot.TeleBot(bot_token)
+
+CHANNEL = ADMINS_CHANNEL_ID
+ADMIN = 6325708298
+
+@bot.message_handler(commands=['start'])
+def send_hello_world(message):
+    
+    bot.send_message(CHANNEL, "hello world")
+
+# Start the bot
+bot.polling()
